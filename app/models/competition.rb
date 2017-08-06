d672f9b1205316cc11796aad74fc22b98734c214
c1505 <coreym247@gmail.com>
2017-08-06 15:10:39 +0000

    basic competition view for 1 user

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -4,14 +4,15 @@ class Competition < ApplicationRecord
   has_many :users, through: :competitions_users
   
   def starting_weight(user)
-    user.weights.where(date: start_date..end_date ).order(date: :asc).first.pounds
+    user.weights.where(date: start_date..end_date ).order(date: :asc).first.try(:pounds)
   end
   
   def last_weight(user)
-    user.weights.where(date: start_date..end_date ).order(date: :asc).last.pounds
+    user.weights.where(date: start_date..end_date ).order(date: :asc).last.try(:pounds)
   end
   
   def percent_change(user)
+    return unless starting_weight(user) && last_weight(user)
     percent = ( starting_weight(user) - last_weight(user) )  / starting_weight(user).round * 100
     percent.round(1)
   end
