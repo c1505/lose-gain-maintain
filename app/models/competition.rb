024d5d8e2bed0287b7b93f49bdf6c8a2f8c93fbc
c1505 <coreym247@gmail.com>
2017-08-09 03:43:30 +0000

    Work in progress.  Competition data formatted for use in chart, but methods could use major refactoring.  Private methods are currently tested

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -11,10 +11,61 @@ class Competition < ApplicationRecord
     user.weights.where(date: start_date..end_date ).order(date: :asc).last.try(:pounds)
   end
 
+  
+  def format_for_graph
+    dates = users.map {|f| f.weights}.flatten.group_by {|f| f.date}.keys.sort
+    hash = {}
+    hash[:dates] = dates
+    percents = add_nil_values.group_by {|f| f.keys}.map do |k, v|
+      { k[0] => v.map {|f| f.values[0]} }
+    end
+    hash[:datasets] = percents
+    hash
+  end
+  
   def percent_change(user)
     return unless starting_weight(user) && last_weight(user)
-    percent = ( last_weight(user) - starting_weight(user) )  / starting_weight(user).round * 100
+    percent = ( last_weight(user) - starting_weight(user) )  / starting_weight(user) * 100
     percent.round(1)
   end
+  
+  private
+  def add_nil_values
+    percent_hash = percent_changes_all
+    dataset = []
+    dates = users.map {|f| f.weights}.flatten.group_by {|f| f.date}.keys.sort.map {|f| f.strftime("%B, %d, %Y") }
+    dates.each do |date|
+      user = 1
+      percent_hash.each do |user_data|
+        email = user_data[0]
+        dataset << {email => user_data[1][date]}
+        user += 1
+      end
+    end
+    dataset
+  end
+  
+  def percent_changes(user)
+    return unless starting_weight(user)
+    starting_weight = starting_weight(user)
+    user_weights = user.weights.where(date: start_date..end_date ).order(date: :asc)
+    hash_of_weights = {}
+    user_weights.each do |weight|
+      percent = ( weight.pounds - starting_weight ) / starting_weight * 100
+      hash_of_weights[weight.date.strftime("%B, %d, %Y")] = percent.round(1)
+    end
+    hash_of_weights
+  end
+  
+
+  
+  def percent_changes_all
+    users.map do |user|
+      [ user.email, percent_changes(user)]
+    end
+  end
+
+  
+
 
 end
--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -54,4 +54,88 @@ RSpec.describe Competition, type: :model do
       expect( competition.percent_change(user) ).to eq (0.6)
     end
   end
+  
+  describe "percent_changes" do
+    it "returns the percent change from the start weight at each time point" do
+      competition = Competition.new(start_date: Time.current - 10.days, end_date: Time.current + 5.minutes)
+      user = User.create(email: "email@email.com", password: "password")
+      date_3 = Time.current.midnight
+      date_1 = date_3 - 2.days
+      date_2 = date_3 - 1.day
+      weight_1 = Weight.new(pounds: 169, date: date_1)
+      weight_2 = Weight.new(pounds: 168, date: date_2)
+      weight_3 = Weight.new(pounds: 170, date: date_3)
+      user.weights = [weight_1, weight_2, weight_3]
+      expect( competition.send(:percent_changes, user) ).to eq ({
+        date_1=> 0.0,
+        date_2=> -0.6,
+        date_3=> 0.6,
+      })
+    end
+  end
+  
+  describe "percent_changes_all" do
+    it "returns the percent change from the start weight at each time point" do
+      competition = Competition.new(start_date: Time.current - 10.days, end_date: Time.current + 5.minutes)
+      user_1 = User.create(email: "email@email.com", password: "password")
+      user_2 = User.create(email: "email2@email.com", password: "password")
+      competition.users = [user_1, user_2]
+      date_3 = Time.current.midnight
+      date_1 = date_3 - 2.days
+      date_2 = date_3 - 1.day
+      date_4 = date_3 - 3.days
+      weight_1 = Weight.new(pounds: 169, date: date_1)
+      weight_2 = Weight.new(pounds: 168, date: date_2)
+      weight_3 = Weight.new(pounds: 170, date: date_3)
+      weight_4 = Weight.new(pounds: 169, date: date_1)
+      weight_5 = Weight.new(pounds: 168, date: date_2)
+      weight_6 = Weight.new(pounds: 170, date: date_4)
+      user_1.weights = [weight_1, weight_2, weight_3]
+      user_2.weights = [weight_6, weight_4, weight_5]
+      
+      expect( competition.send(:percent_changes_all) ).to eq ([ 
+        [ "email@email.com", 
+          { 
+            date_1=> 0.0,
+            date_2=> -0.6,
+            date_3=> 0.6,
+          }
+        ],
+        [ "email2@email.com", 
+          { 
+            date_4=> 0.0,
+            date_1=> -0.6,
+            date_2=> -1.2,
+          }
+        ]
+      ])
+    end
+  end
+  describe "format for chart" do 
+    it "groupes by date and fills in null values where no percent exist for a given date" do 
+      competition = Competition.new(start_date: Time.current - 10.days, end_date: Time.current + 5.minutes)
+      user_1 = User.create(email: "email@email.com", password: "password")
+      user_2 = User.create(email: "email2@email.com", password: "password")
+      competition.users = [user_1, user_2]
+      date_3 = Time.current.midnight
+      date_1 = date_3 - 2.days
+      date_2 = date_3 - 1.day
+      date_4 = date_3 - 3.days
+      weight_1 = Weight.new(pounds: 169, date: date_1)
+      weight_2 = Weight.new(pounds: 168, date: date_2)
+      weight_3 = Weight.new(pounds: 170, date: date_3)
+      weight_4 = Weight.new(pounds: 169, date: date_1)
+      weight_5 = Weight.new(pounds: 168, date: date_2)
+      weight_6 = Weight.new(pounds: 170, date: date_4)
+      user_1.weights = [weight_1, weight_2, weight_3]
+      user_2.weights = [weight_6, weight_4, weight_5]
+      
+      expect( competition.format_for_graph ).to eq (
+        {
+          "dates": [date_4, date_1, date_2, date_3 ],
+          "datasets": [{"email@email.com"=>[nil, 0.0, -0.6, 0.6]}, {"email2@email.com"=>[0.0, -0.6, -1.2, nil]}]
+        }
+      )
+    end
+  end
 end
