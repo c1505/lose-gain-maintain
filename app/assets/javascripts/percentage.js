422aaea98100c8caff106134d5af54ea8c96f231
c1505 <coreym247@gmail.com>
2017-08-10 12:35:14 +0000

    fixed issue with chart not rending on iOS safari.  Object.values() was the problem

--- app/assets/javascripts/percentage.js
+++ app/assets/javascripts/percentage.js
@@ -5,33 +5,38 @@ $(".competitions.show").ready(function() {
     var dates = data.dates.map(function(x) {
       return x.split("T")[0]
     })
-    drawPercentsChart(data, dates);
+      var vals1 = Object.keys(data.datasets[0]).map(function(key) {
+    return data.datasets[0][key];
+    });
+    var vals2 = Object.keys(data.datasets[1]).map(function(key) {
+      return data.datasets[1][key];
+    });
+    drawPercentsChart(data, dates, vals1, vals2);
   })
 });
 
-function drawPercentsChart(data,dates) {
+function drawPercentsChart(data,dates, vals1, vals2) {
   var ctx = document.getElementById("percentage").getContext('2d');
+
   var myChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: dates,
         datasets: [{
             label: Object.keys(data.datasets[0])[0],
-            data: Object.values(data.datasets[0])[0],
+            data: vals1[0],
             fill: false,
             spanGaps: true,
             backgroundColor: 'rgba(240,50,50,1)',
-          
             borderColor: 'rgba(240,50,50,1)',
             borderWidth: 1
         }, {
           label: Object.keys(data.datasets[1])[0],
-          data: Object.values(data.datasets[1])[0],
+          data: vals2[0],
           fill: false,
           spanGaps: true,
           backgroundColor: 'rgba(55,55,189,1)',
           borderColor: 'rgba(55,55,189,1)',
-          
           borderWidth: 1
       }]
     },
