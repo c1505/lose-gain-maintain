ac70c8b16d9b897cc49306329609d4581362a1d4
c1505 <coreym247@gmail.com>
2017-09-25 20:01:07 +0000

    goal weekly targets

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,16 @@
 class Goal < ApplicationRecord
   belongs_to :user
+  
+  def weekly_target_change
+    last_7_days_average =  Weight.average(7, user).round(1)
+    time_for_change = target_date - Time.current
+    time_for_change = to_weeks(time_for_change)
+    total = (weight - last_7_days_average) / time_for_change
+    total.round(2)
+  end
+  
+  private
+  def to_weeks(seconds)
+    seconds / 60 / 60 / 24 / 7
+  end
 end
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Goal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#weekly_target_change" do
+    it "returns weekly target change" do
+      goal = Goal.new(weight: 170, start_date: Time.current, target_date: Time.current + 14.days)
+      user = User.create(email: "email@email.com", password: "password")
+      weight_1 = Weight.new(pounds: 171, date: Time.current)
+      weight_2 = Weight.new(pounds: 171, date: Time.current - 2.days)
+      weight_3 = Weight.new(pounds: 171, date: Time.current - 1.day)
+      user.weights = [weight_1, weight_2, weight_3]
+      user.goals << goal
+      
+      expect(goal.weekly_target_change).to eq -0.5
+    end
+  end
 end
